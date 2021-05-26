4aaf0e90293707031a792b528eb73bedb193212f
zizynonno <zizynonno@gmail.com>
2021-05-27 00:50:47 +0900

    add: モデル層の練習

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
   # GET /books or /books.json
   def index
     @books = Book.all
+    # @books = Book.find([2,5,10])
   end
 
   # GET /books/1 or /books/1.json
